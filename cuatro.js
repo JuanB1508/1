085f106d3381ef119334422db4b2ee11a91ec288
JuanB1508 <barreajuanjose@gmail.com>
2023-05-11 17:41:11 -0300

    Git & GitHub

---
+++ cuatro.js
@@ -0,0 +1,11 @@
+var git = {
+    software: true,
+    plataforma: false,
+    version: "git version 2.39.2 (Apple Git-143)"
+};
+
+var gitHub = {
+    software: false,
+    plataforma: true,
+    miusuario: "JuanB1508"
+};
